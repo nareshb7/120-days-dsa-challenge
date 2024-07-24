1cbcd420d21e699387f0d2dc4ff34f03ebf69247
nareshb7 <nareshbjava7@gmail.com>
2024-07-24 23:07:00 +0530

    Day04 rotate array new approach added

--- Day04/Notes.md
+++ Day04/Notes.md
@@ -2,7 +2,14 @@
 
 ## Approach
 
-### 1
+
+### 1 T.C - O(n) , S.C - O(1)
+- first we need to get the `k` value by doing `k % nums.length`(to handle cases where k is greater than array length. 
+- reverse the whole array using two pointer approach
+- now the rotate part will come to the first , so reverse that part
+- reverse the remining part 
+
+### 2 T.C - O(n) , S.C - O(k)
 - first we need to get the `k` value by doing `k % nums.length`(to handle cases where k is greater than array length.
 ), we will get the `k` value in the range of nums length
 - extarct the rotate part of array by using splice `const rotatePart = nums.splice(nums.length -k)`, now we will get roatting part of array
@@ -11,7 +18,7 @@
 ## Complexity
 
 - Time complexity: O(n)
-- Space complexity: O(k)
+- Space complexity: O(1)
 
 
 ## Key Learnings
--- Day04/Solution.js
+++ Day04/Solution.js
@@ -3,16 +3,30 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
- const rotate = (nums, k) => {
-  k =  k% nums.length
-  
-  // we are splicing the last part of the array using k
 
-  let end = nums.splice(-k, k)
-  // add the last part in the starting of nums array
-  nums.splice(0, 0, ...end)
-  return nums
-  
+const reverse = (arr, start, end) => {
+  while (start < end) {
+    const swap = arr[start];
+    arr[start] = arr[end];
+    arr[end] = swap;
+    start++;
+    end--;
+  }
+};
+const rotate = (nums, k) => {
+  k = k % nums.length;
+  reverse(nums, 0, nums.length - 1);// revese whole array - [7,6,5,4,3,2,1]
+  reverse(nums, 0, k - 1); // reverse the k part - [5,6,7,1,2,3,4]
+  reverse(nums, k, nums.length - 1); // reverse the remaining part - [5,6,7,1,2,3,4]
+  return nums;
+
+  // k =  k% nums.length
+  // // we are splicing the last part of the array using k
+  // let end = nums.splice(-k, k)
+  // // add the last part in the starting of nums array
+  // nums.splice(0, 0, ...end)
+  // return nums
+
   // reverse the array
   // let l =0, r = nums.length-1
   // while (l< r) {
@@ -40,17 +54,14 @@
   //     l++
   //     r--
   // }
-  
 
   // just popping the last item and assigning in start of the array
   // for  (let i=0;i<k; i++) {
   //     nums.unshift(nums.pop())
   // }
-  
-  
 };
 
-
-const nums = [1,2,3,4,5,6,7], k = 3
+const nums = [1, 2, 3, 4, 5, 6, 7],
+  k = 3;
 // Output: [5,6,7,1,2,3,4]
-console.log('Rotate Array::', rotate(nums, k))
+console.log("Rotate Array::", rotate(nums, k));

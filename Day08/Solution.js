2763fb61e07aa97e25b96e79ee26f3a9c325821e
nareshb7 <nareshbjava7@gmail.com>
2024-07-27 22:46:04 +0530

    Day08: Solution updated

--- Day08/Solution.js
+++ Day08/Solution.js
@@ -6,27 +6,16 @@
  var subarraySum = function(nums, k) {
   let hashMap = new Map()
   hashMap.set(0,1)
-  let res =0
+  let result =0
   let sum =0
   for (let i=0; i<nums.length; i++) {
       sum+=nums[i]
       if(hashMap.has(sum-k)) {
-          res+= hashMap.get(sum-k)
+          result+= hashMap.get(sum-k)
       }
       hashMap.set(sum, (hashMap.get(sum) || 0) + 1);
   }
-  return res
-  // let hashMap ={0: 1}
-  // 
-  // for (let i=0; i< nums.length; i++) {
-  //     sum+=nums[i]
-  //     if(sum-k in hashMap){
-  //         res+= hashMap[sum-k]
-  //     }
-  //     hashMap[sum] = hashMap[sum] ? hashMap[sum]+ 1: 1
-  // }
-  // return res
-  
+  return result  
 };
 
 const arr = [1,1,1]

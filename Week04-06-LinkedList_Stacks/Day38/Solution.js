7b2d7a3dcab15da3a9e6271e12959a9f43309d31
nareshb7 <nareshbjava7@gmail.com>
2024-08-25 21:45:11 +0530

    Day 38: Car Fleet

--- Week04-06-LinkedList_Stacks/Day38/Solution.js
+++ Week04-06-LinkedList_Stacks/Day38/Solution.js
@@ -4,18 +4,23 @@
  * @param {number[]} speed
  * @return {number}
  */
- var carFleet = function(target, position, speed) {
-  const pair = position.map((_, i) => [position[i], (target- position[i])/speed[i] ]).sort((a, b) => b[0] - a[0])
-  const stack =[]
+var carFleet = function (target, position, speed) {
+  const pair = position
+    .map((_, i) => [position[i], (target - position[i]) / speed[i]])
+    .sort((a, b) => b[0] - a[0]);
+  const stack = [];
   for (const [_, time] of pair) {
-      stack.push(time)
-      if (stack.length >= 2 && stack[stack.length -1] <= stack[stack.length -2]) {
-          stack.pop()
-      }
+    stack.push(time);
+    if (
+      stack.length >= 2 &&
+      stack[stack.length - 1] <= stack[stack.length - 2]
+    ) {
+      stack.pop();
+    }
   }
-  return stack.length
+  return stack.length;
 };
 
-const target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
-console.log('fleet::', carFleet(target, position, speed))
+const target = 12,position = [10, 8, 0, 5, 3], speed = [2, 4, 1, 1, 3];
+console.log("fleet::", carFleet(target, position, speed));
 // Output: 3

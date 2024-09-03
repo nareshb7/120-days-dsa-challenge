caaa8cc673bdb3c6965aea80ed813c2a18c4cd17
nareshb7 <nareshbjava7@gmail.com>
2024-09-03 22:12:44 +0530

    Day 45: Invert Binary Tree

--- Week07-09-Trees_Graphs/Day45/Solution.js
+++ Week07-09-Trees_Graphs/Day45/Solution.js
@@ -24,5 +24,5 @@ var invertTree = function (root) {
 };
 
 const root = [4, 2, 7, 1, 3, 6, 9];
-console.log("Inverted Tree::", arrayToBinaryTree(root));
+console.log("Inverted Tree::", invertTree(arrayToBinaryTree(root)));
 // Output: [4,7,2,9,6,3,1]

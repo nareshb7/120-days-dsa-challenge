db8b9658e15dea3e8cfa4c1c1899418eff5bd4bf
nareshb7 <nareshbjava7@gmail.com>
2024-09-10 22:29:35 +0530

    Day 53: Binary Tree Postorder Traversal

--- Week07-09-Trees_Graphs/Day53/Solution.js
+++ Week07-09-Trees_Graphs/Day53/Solution.js
@@ -14,18 +14,36 @@
 const { arrayToBinaryTree } = require("..");
 
 // Left -> Right -> Root (LRR)
-var postorderTraversal = function(root) {
-  const result =[]
-  const traverse =(node) => {
-      if (!node) return false;
-      traverse(node.left)
-      traverse(node.right)
-      result.push(node.val)
-  }
-  traverse(root)
-  return result
+var postorderTraversal = function (root) {
+  const result = [];
+  const traverse = (node) => {
+    if (!node) return false;
+    traverse(node.left);
+    traverse(node.right);
+    result.push(node.val);
+  };
+  traverse(root);
+  return result;
 };
 
-const root = [1,null,2,3]
-console.log("Postorder Tree::", postorderTraversal(arrayToBinaryTree(root)))
+const root = [1, null, 2, 3];
+console.log("Postorder Tree::", postorderTraversal(arrayToBinaryTree(root)));
 // Output: [3,2,1]
+
+
+// var postorderTraversal = function(root) {
+//   if (!root) return [];
+//   const stack = [root];
+//   const result = [];
+  
+//   while (stack.length > 0) {
+//       const node = stack.pop();
+//       result.push(node.val);
+      
+//       // Push left first, so right is processed before left
+//       if (node.left) stack.push(node.left);
+//       if (node.right) stack.push(node.right);
+//   }
+  
+//   return result.reverse(); // Reverse to get post-order traversal
+// };

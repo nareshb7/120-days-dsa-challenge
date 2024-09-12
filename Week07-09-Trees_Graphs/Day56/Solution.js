e4a7aaaaf7922a8f0c7704b1513861fde69b6c1c
nareshb7 <nareshbjava7@gmail.com>
2024-09-12 23:06:26 +0530

    Day 56: Construct BT from Preorder and Inorder

--- Week07-09-Trees_Graphs/Day56/Solution.js
+++ Week07-09-Trees_Graphs/Day56/Solution.js
@@ -43,4 +43,5 @@ var buildTree = function(preorder, inorder) {
 };
 
 const preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+console.log("Binary Tree::", buildTree(preorder, inorder))
 // Output: [3,9,20,null,null,15,7]

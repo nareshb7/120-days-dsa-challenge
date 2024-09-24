5289daa2508986fd815aa1bec5b5815256b94746
nareshb7 <nareshbjava7@gmail.com>
2024-09-24 23:11:44 +0530

    Day 67 solution update

--- Week10-11-Recursion_DP/Day67/Solution.js
+++ Week10-11-Recursion_DP/Day67/Solution.js
@@ -2,20 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
- var lengthOfLIS = function(nums) {
-    if(nums.length === 0) return 0
-    let dp = new Array(nums.length).fill(1)
-    for(let i = 1; i < dp.length; i++) {
-        for(let j = 0; j < i; j++) {
-            if(nums[j] < nums[i]){
-                dp[i] = Math.max(dp[i], 1 + dp[j])
-            }
-        }
+var lengthOfLIS = function (nums) {
+  if (nums.length === 0) return 0;
+  let dp = new Array(nums.length).fill(1);
+  for (let i = 1; i < dp.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], 1 + dp[j]);
+      }
     }
-    console.log(dp)
-    return Math.max(...dp)
+  }
+  return Math.max(...dp);
 };
 
-const nums = [10,9,2,5,3,7,101,18]
-console.log("length of LIS::", lengthOfLIS(nums))
+const nums = [10, 9, 2, 5, 3, 7, 101, 18];
+console.log("length of LIS::", lengthOfLIS(nums));
 // Output: 4

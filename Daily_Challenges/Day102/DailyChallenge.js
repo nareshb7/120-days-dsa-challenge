50c84173f29fe96b3df5ddeeb7cf34639140151e
nareshb7 <nareshbjava7@gmail.com>
2024-11-02 18:24:39 +0530

    2490. Circular Sentence

---
+++ Daily_Challenges/Day102/DailyChallenge.js
@@ -0,0 +1,71 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+// var isCircularSentence = function (sentence) {
+//   const words = sentence.split(" ");
+// //   if (words.length === 1) {
+// //     const word = words[0];
+// //     return word[0] === word[word.length - 1];
+// //   }
+//   let lastWord = words[0];
+//   for (let i = 1; i < words.length; i++) {
+//     const word = words[i];
+
+//     if (lastWord[lastWord.length - 1] === word[0]) {
+//       lastWord = word;
+//     } else {
+//       return false;
+//     }
+//   }
+//   if (lastWord[lastWord.length - 1] !== words[0][0]) {
+//     return false;
+//   }
+//   return true;
+// };
+var isCircularSentence = function (sentence) {
+  const n = sentence.length;
+  for (let i = 0; i < n; i++) {
+    const char = sentence[i];
+    if (char === " ") {
+      if (sentence[i - 1] !== sentence[i + 1]) {
+        return false;
+      } else {
+        continue;
+      }
+    }
+    // for last character
+    if (i === n - 1) {
+      if (sentence[i] !== sentence[0]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+// 2490. Circular Sentence
+
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+// For example, "Hello World", "HELLO", "hello world hello world" are all sentences.
+// Words consist of only uppercase and lowercase English letters. Uppercase and lowercase English letters are considered different.
+
+// A sentence is circular if:
+
+// The last character of a word is equal to the first character of the next word.
+// The last character of the last word is equal to the first character of the first word.
+// For example, "leetcode exercises sound delightful", "eetcode", "leetcode eats soul" are all circular sentences. However,
+// "Leetcode is cool", "happy Leetcode", "Leetcode" and "I like Leetcode" are not circular sentences.
+
+// Given a string sentence, return true if it is circular. Otherwise, return false.
+
+// Example
+// Input: sentence = "leetcode exercises sound delightful"
+// Output: true
+// Explanation: The words in sentence are ["leetcode", "exercises", "sound", "delightful"].
+// - leetcode's last character is equal to exercises's first character.
+// - exercises's last character is equal to sound's first character.
+// - sound's last character is equal to delightful's first character.
+// - delightful's last character is equal to leetcode's first character.
+// The sentence is circular.

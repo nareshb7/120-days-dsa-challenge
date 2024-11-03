892c64cf024e6a6bed7a0dc5017ce89fcd9faa48
nareshb7 <nareshbjava7@gmail.com>
2024-11-03 22:22:08 +0530

    Day 103: new approach added

--- Daily_Challenges/Day103/DailyChallenge.js
+++ Daily_Challenges/Day103/DailyChallenge.js
@@ -3,17 +3,40 @@
  * @param {string} goal
  * @return {boolean}
  */
- var rotateString = function(s, goal) {
+//  var rotateString = function(s, goal) {
+//   if (s.length !== goal.length) return false;
+//   // let str = s;
+//   // const n = s.length
+//   // for (let i=0; i<n ; i++) {
+//   //     if (str === goal) return true;
+//   //     str = s.substr(i)+ s.substr(0,i)
+//   // }
+//   // if (str === goal) return true;;
+//   // return false;
+//   return (s+s).includes(goal)
+// };
+
+const includesGoal =(text, goal) => {
+  const n = text.length;
+  const m = goal.length;
+  
+  for (let i=0; i<n-m; i++) {
+      let found = true;
+      
+      for (let j=0; j<m; j++) {
+          if (text[i+j] !== goal[j]) {
+              found = false;
+              break;
+          }
+      }
+      if (found) return true;
+  }
+  return false
+}
+
+var rotateString = function(s, goal) {
   if (s.length !== goal.length) return false;
-  // let str = s;
-  // const n = s.length
-  // for (let i=0; i<n ; i++) {
-  //     if (str === goal) return true;
-  //     str = s.substr(i)+ s.substr(0,i)
-  // }
-  // if (str === goal) return true;;
-  // return false;
-  return (s+s).includes(goal)
+  return includesGoal(s+s, goal)
 };
 
 // 796. Rotate String

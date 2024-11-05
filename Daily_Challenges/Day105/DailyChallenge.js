2b62c72f4203c26599973bfed0112d849b1b5258
nareshb7 <nareshbjava7@gmail.com>
2024-11-05 22:11:08 +0530

    Min no of Changes to Make Binary String Beautiful

--- Daily_Challenges/Day105/DailyChallenge.js
+++ Daily_Challenges/Day105/DailyChallenge.js
@@ -2,16 +2,15 @@
  * @param {string} s
  * @return {number}
  */
- var minChanges = function(s) {
-    let count =0;
-      for (let i=0; i< s.length ; i= i+2) {
-          if (s[i] != s[i+1]) count++;
-      }
-      return count
-      
-  };
+var minChanges = function (s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i = i + 2) {
+    if (s[i] != s[i + 1]) count++;
+  }
+  return count;
+};
 
-  // 2914. Minimum Number of Changes to Make Binary String Beautiful
+// 2914. Minimum Number of Changes to Make Binary String Beautiful
 
 // You are given a 0-indexed binary string s having an even length.
 // A string is beautiful if it's possible to partition it into one or more substrings such that:
@@ -19,7 +18,6 @@
 // Each substring contains only 1's or only 0's.
 // You can change any character in s to 0 or 1.
 // Return the minimum number of changes required to make the string s beautiful.
-
 
 // Example:
 // Input: s = "1001"
--- Daily_Challenges/Day105/Gfg.js
+++ Daily_Challenges/Day105/Gfg.js
@@ -3,24 +3,23 @@
  */
 
 class Solution {
-    rotate(mat) {
-        // code here
-        const n = mat.length;
-        const m = mat[0].length;
-        
-        for  (let i=0; i< n ; i++) {
-            for (let j=i+1; j<m; j++) {
-                [mat[j][i], mat[i][j]] = [mat[i][j], mat[j][i]]
-            }
-        }
-        
-        for (let i=0; i<n;  i++) {
-            mat[i].reverse()
-        }
-        return mat
-    }
-}
+  rotate(mat) {
+    // code here
+    const n = mat.length;
+    const m = mat[0].length;
 
+    for (let i = 0; i < n; i++) {
+      for (let j = i + 1; j < m; j++) {
+        [mat[j][i], mat[i][j]] = [mat[i][j], mat[j][i]];
+      }
+    }
+
+    for (let i = 0; i < n; i++) {
+      mat[i].reverse();
+    }
+    return mat;
+  }
+}
 
 // Rotate by 90 degree
 
@@ -28,6 +27,6 @@ class Solution {
 
 // Input: mat[][] = [[1 2 3], [4 5 6], [7 8 9]]
 // Output:
-// 7 4 1 
+// 7 4 1
 // 8 5 2
 // 9 6 3
